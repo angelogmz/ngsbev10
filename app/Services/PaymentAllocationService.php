2f23f5271336aeda468ed9b94bff79c972dfc609
AngeloGomez <angelowebsolutions@gmail.com>
2026-04-21 00:38:14 +0100

    removed allocation

--- app/Services/PaymentAllocationService.php
+++ app/Services/PaymentAllocationService.php
@@ -112,7 +112,7 @@ class PaymentAllocationService
     // header('Access-Control-Allow-Origin: http://localhost:5173');
     // header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
     // header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-        try {
+        /*try {
             $contractDetails = $this->getContractDetails($contract_no);
 
             // Decode the JSON content
@@ -494,7 +494,7 @@ class PaymentAllocationService
         } catch (\Exception $e) {
             // Log error and skip
             return "Failed to allocate payments for contract {$contract_no}: " . $e->getMessage();
-        }
+        }*/
     }
 
 }
